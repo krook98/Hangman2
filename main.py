35aae3b6a9e21c9834213b53baab554cc7dcabea
sokal <sokalski.jakub98@gmail.com>
2021-11-03 15:38:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+from words import word_list
+from art import logo
+from art import stages
+
+print(logo)
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+lives = 6
+end_of_game = False
+
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for letter in chosen_word:
+    display.append("_")
+print(display)
+
+
+while not end_of_game:
+    guess = input("Please guess the letter").lower()
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    if guess not in chosen_word:
+        lives -= 1
+        print(stages[lives])
+        if lives == 0:
+            print("You lost!")
+            end_of_game = True
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+
+    print(display)
+
